faf9582eefb45feeaba7e03cf9fe53c78f459b0e
neikei <niklas@vosskoetter.org>
2017-08-11 20:51:47 +0200

    Improvements

--- ansible/tools/install_ansible_in_Vagrantbox.sh
+++ ansible/tools/install_ansible_in_Vagrantbox.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-debian_servion=`cat /etc/debian_version`
+debian_version=`cat /etc/debian_version`
 ansible_installed=`which ansible | wc -l`
 
-if [ $ansible_installed -eq 0 ] && [[ $debian_servion =~ ^8.*$ ]]; then
+if [ $ansible_installed -eq 0 ] && [[ $debian_version =~ ^8.*$ ]]; then
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   sudo apt-get update -y
   sudo apt-get install -y ansible
-elif [ $ansible_installed -eq 0 ] && [[ $debian_servion =~ ^9.*$ ]]; then
+elif [ $ansible_installed -eq 0 ] && [[ $debian_version =~ ^9.*$ ]]; then
   sudo apt-get update -y
   sudo apt-get install -y ansible
 else
