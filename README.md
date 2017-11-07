78e7d4d7ceadeceb0c27eafb2c41e66d53981f12
neikei <niklas@vosskoetter.org>
2017-11-07 18:12:42 +0100

    Improved Ansible installation and pinned version 2.4.1.0

--- README.md
+++ README.md
@@ -92,6 +92,12 @@ Feel free to report issues, fork this project and submit pull requests.
 
 ## Changelog
 
+07 November 2017
+
+- Added php-apcu and php-memcache packages
+- Improved Ansible installation
+- Hardcoded Ansible version to 2.4.1.0
+
 06 November 2017
 
 - Removed Varnish
--- ansible/tools/install_ansible_in_Vagrantbox.sh
+++ ansible/tools/install_ansible_in_Vagrantbox.sh
@@ -2,14 +2,20 @@
 debian_version=`cat /etc/debian_version`
 ansible_installed=`which ansible | wc -l`
 
-if [ $ansible_installed -eq 0 ] && [[ $debian_version =~ ^8.*$ ]]; then
-  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+if [ $ansible_installed -eq 0 ]; then
+
+  # Install dirmngr if Debian major version is 9
+  if [[ $debian_version =~ ^9.*$ ]]; then
+    sudo apt install -y dirmngr
+  fi
+
+  # Add ansible repository, install ansible 2.4.1.0 and set it on hold
+  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu xenial main" >> /etc/apt/sources.list.d/ansible.list
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  sudo apt-get update -y
-  sudo apt-get install -y ansible
-elif [ $ansible_installed -eq 0 ] && [[ $debian_version =~ ^9.*$ ]]; then
-  sudo apt-get update -y
-  sudo apt-get install -y ansible
+  sudo apt update -y
+  sudo apt install -y ansible=2.4.1.0-1ppa~xenial
+  sudo apt-mark hold ansible
+
 else
   echo "Ansible is already installed."
 fi
